7b689e9d8495de6df55ef66064d87ee1ee726408
Duaa Braik <duaa.braik@yum.com>
2026-03-12 11:41:20 +0200

    First commit

---
+++ ApprovalSystem/Controllers/DataController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApprovalSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        private readonly ILogger<DataController> _logger;
+
+        public DataController(ILogger<DataController> logger)
+        {
+            _logger = logger;
+        }        
+    }
+}
