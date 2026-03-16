b8608f8126f01ee9f33516fdb939aa38cbbed341
DuaaBraik <dbraik@itgsoftware.com>
2026-03-16 02:38:10 +0200

    Modify Program.cs file to use swagger for production

--- ApprovalSystem/Program.cs
+++ ApprovalSystem/Program.cs
@@ -15,11 +15,11 @@ builder.Services.AddScoped<ISharePointService, SharePointService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
